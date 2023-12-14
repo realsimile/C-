33f40334027d0f3f576e03d030714884de755ec4
realsimile <150151013+realsimile@users.noreply.github.com>
2023-12-14 16:02:08 +0400

    Create StarterProgram.cpp

---
+++ StarterProgram.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+//sort(a, a + n); a = {1, 2, 3, 5, 9};
+
+void solve(){
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	return 0;
+}
