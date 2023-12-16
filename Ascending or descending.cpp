10310779827be07adb4d80dd8a8e018c53ffe95e
realsimile <150151013+realsimile@users.noreply.github.com>
2023-12-16 13:55:01 +0400

    Update Ascending or descending.cpp
    Remade the program to have a function so its easier to implement in other programs

--- Ascending or descending.cpp
+++ Ascending or descending.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-int main(){
-	int n; cin>>n;
-	int a[n]; int dp[n][2];
+vector <int> a; int n;
+
+bool IsDescendingOrAscending(){
+	int dp[n][2];
 	
 	for (int k = 0; k < n; k++){
-		cin>>a[k];
 		dp[k][0] = 1;
 		dp[k][1] = 1;
-	} 
+	}
 	
 	for (int k = 1; k < n; k++){
 		if (a[k] >= a[k - 1]) dp[k][0] = dp[k - 1][0] + 1;
@@ -25,7 +25,17 @@ int main(){
 	
 	for (int k = 0; k < n; k++) mx = max({dp[k][0], dp[k][1], mx});
 	
-	if (mx == n) cout<<"Array is ascending/descending";
+	return mx == n;
+}
+
+int main(){
+	int n; cin>>n;
+	
+	a.resize(n + 1);
+	
+	for (int k = 0; k < n; k++) cin>>a[k];
+	
+	if (IsDescendingOrAscending()) cout<<"Array is ascending/descending";
 	else cout<<"Array is not ascending/descending";
 	
 	return 0;
